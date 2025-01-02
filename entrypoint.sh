79d2b16a1211bc1aeb5221240bd871cc38c517a0
Zixties60 <13465511+Zixties60@users.noreply.github.com>
2025-01-02 11:32:44 +0700

    feat: add support PUID PGID

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,7 @@ VERBOSE=""
 echo "Live channel: [$CHANNEL_URL]"
 echo "Output video file name: [$OUTPUT_VIDEO]"
 echo "Verbose: [$YTARCHIVE_VERBOSE]" 
+echo "User PUID: [$PUID], Group PGID: [$PGID]"
 
 # # Check if CHANNEL_URL is provided
 if [ "$CHANNEL_URL" = "" ]; then
@@ -24,6 +25,10 @@ if [ "$YTARCHIVE_VERBOSE" = "true" ]; then
     VERBOSE="--verbose --debug"
 fi
 
+# Ensure PUID and PGID are set; default to root if not provided
+PUID=${PUID:-0}
+PGID=${PGID:-0}
+
 echo "        _                       _      _              "
 echo "       | |                     | |    (_)             "
 echo " _   _ | |_   __ _  _ __   ___ | |__   _ __   __  ___ "
@@ -34,4 +39,17 @@ echo "  __/ |                                               "
 echo " |___/                                                "
 
 # Run ytarchive with the remaining arguments
-ytarchive $VERBOSE --monitor-channel -w -r 600 --directory-permissions 0777 --file-permissions 0666 --merge --mkv --add-metadata --write-description --write-thumbnail --newline -o "$OUTPUT_VIDEO" $CHANNEL_URL best
+su-exec $PUID:$PGID ytarchive $VERBOSE \
+    --monitor-channel \
+    --wait \
+    --retry-stream 600 \
+    --directory-permissions 0777 \
+    --file-permissions 0666 \
+    --merge \
+    --mkv \
+    --add-metadata \
+    --write-description \
+    --write-thumbnail \
+    --newline \
+    --output "$OUTPUT_VIDEO" $CHANNEL_URL best
+
